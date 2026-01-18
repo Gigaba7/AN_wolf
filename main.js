db522ef44308dc14fa132b82ea2eebb22006c89a
Gigaba7 <gigaba.human@gmail.com>
2026-01-18 17:22:49 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // アークナイツ人狼 ツール エントリーポイント
 
 // バージョン情報
-const APP_VERSION = "1.1.63";
+const APP_VERSION = "1.1.67";
 
 import { GameState } from "./game-state.js";
 import { onAuthStateChanged, signInAnonymously } from "./firebase-auth.js";
